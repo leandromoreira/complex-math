b638cd5da6b3061d0e90acfc8321d662a7002d73
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2013-05-30 03:47:09 +0200

    mocking with mock python

--- tests/test_simple_math.py
+++ tests/test_simple_math.py
@@ -7,10 +7,17 @@ def test_simple_math():
   add(1, 3).should.be.equals(4)
 
 #Stubing
-def test_simple_math_remotely():
+def test_simple_math_remotely_stubed():
   server = Mock()
   server.computes_add = Mock(return_value=3)
 
   add_remotely(1, 2, server).should.be.equals(3)
 
+#Mocking
+def test_simple_math_remotely_mocked():
+  server = Mock()
+
+  add_remotely(1, 2, server)
+
+  server.computes_add.assert_called_once_with(1, 2)
 
