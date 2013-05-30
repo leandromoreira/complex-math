06e5838355f5a1feab1ebb3c1ec2796631adfb96
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2013-05-30 04:08:53 +0200

    adds test stubing internal dependency

--- cmath/simple_math.py
+++ cmath/simple_math.py
@@ -1,4 +1,5 @@
 import mailer
+import nasa
 
 def add(x, y):
   return x + y
@@ -10,4 +11,8 @@ def add_and_sends_email(x, y):
   z = x + y
   mailer.send(to='master@math.com', subject='Complex addition', body=('The result was %s' % z))
   return z
+
+def add_and_sum_with_rnd(x, y):
+  z = x + y
+  return nasa.random() + z
 
--- tests/test_simple_math.py
+++ tests/test_simple_math.py
@@ -1,7 +1,7 @@
 from mock import Mock, patch
 import sure
 
-from cmath.simple_math import add, add_remotely, add_and_sends_email
+from cmath.simple_math import add, add_remotely, add_and_sends_email, add_and_sum_with_rnd
 
 def test_simple_math():
   add(1, 3).should.be.equals(4)
@@ -20,6 +20,13 @@ def test_simple_math_remotely_mocked():
   add_remotely(1, 2, server)
 
   server.computes_add.assert_called_once_with(1, 2)
+
+#Stubing an internal dependency
+@patch('cmath.nasa.random')
+def test_simple_math_with_randon_generated_by_nasa(nasa_random_generator):
+  nasa_random_generator.configure_mock(return_value=42)
+
+  add_and_sum_with_rnd(3, 9).should.be.equals(54)
 
 #Mocking an internal dependency
 @patch('mailer.send')
