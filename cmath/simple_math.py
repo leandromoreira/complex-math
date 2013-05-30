28750ec89404c76ad94c9561dd429f5b4b2d4f9c
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2013-05-30 03:40:46 +0200

    stubing with mock python

--- cmath/simple_math.py
+++ cmath/simple_math.py
@@ -1,3 +1,6 @@
 def add(x, y):
   return x + y
 
+def add_remotely(x, y, server):
+  return server.computes_add(x, y)
+
--- tests/test_simple_math.py
+++ tests/test_simple_math.py
@@ -1,8 +1,16 @@
-
+from mock import Mock
 import sure
 
-from cmath.simple_math import add
+from cmath.simple_math import add, add_remotely
 
 def test_simple_math():
   add(1, 3).should.be.equals(4)
+
+#Stubing
+def test_simple_math_remotely():
+  server = Mock()
+  server.computes_add = Mock(return_value=3)
+
+  add_remotely(1, 2, server).should.be.equals(3)
+
 
